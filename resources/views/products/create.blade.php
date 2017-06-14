20b460f17e2083a721d5ee9d33033b4cdfac5398
fabel <ilanroest@hotmail.com>
2017-06-14 11:19:02 +0200

    Hotfix create cart

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <form method="POST" action="/products">
+                <form method="POST" action="/products" enctype='multipart/form-data'>
                     {{ csrf_field() }}
 
                     <div class="form-group">
