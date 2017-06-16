ffc6eb5838766bd75511f60499657dbde790485a
MagicMilan <milanbakker@hotmail.com>
2017-06-16 13:42:54 +0200

    create push

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -23,7 +23,7 @@
                         <label for="category">Categorie</label>
                         <select id="category">
                             @foreach($categories as $category)
-                                <option value="volvo">{{ $category->name }}</option>
+                                <option value="category">{{ $category->name }}</option>
                             @endforeach
                         </select>
                     </div>
